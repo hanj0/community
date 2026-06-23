5c96e1f3d228304fe5632d82e7f8375b902f8535
hanj0 <hanjaeyoung0@gmail.com>
2026-06-23 18:46:51 +0900

    Chore: 사용하지 않는 import 제거

--- backend/src/main/java/com/han/community/entity/CommentReaction.java
+++ backend/src/main/java/com/han/community/entity/CommentReaction.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @NoArgsConstructor
--- backend/src/main/java/com/han/community/repository/CommentReactionRepository.java
+++ backend/src/main/java/com/han/community/repository/CommentReactionRepository.java
@@ -2,8 +2,6 @@ package com.han.community.repository;
 
 import com.han.community.entity.CommentReaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
